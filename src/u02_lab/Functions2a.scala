bcbbeeff646e3006206a9de89471e920de61343a
Fabio Muratori <fabio.muratori2@studio.unibo.it>
2021-03-08 18:55:04 +0100

    Implement and test task 2a

---
+++ src/u02_lab/Functions2a.scala
@@ -0,0 +1,26 @@
+package u02_lab
+
+import org.junit.jupiter.api.Assertions._
+import org.junit.jupiter.api.Test
+
+object Functions2a {
+
+  val parity: Int => String = (x:Int) => {
+    parityFunction(x)
+  }
+
+  def parityFunction (x:Int): String = x match {
+    case x if (x%2 == 0) => "even"
+    case _ => "odd"
+  }
+
+  @Test def testParity(){
+    assertEquals("even", parity(4))
+    assertEquals("odd", parity(5))
+  }
+
+  @Test def testParityFunction(){
+    assertEquals("even", parityFunction(4))
+    assertEquals("odd", parityFunction(5))
+  }
+}
---
+++ src/u02_lab/Neg2b.scala
@@ -0,0 +1,8 @@
+package u02_lab
+
+import org.junit.jupiter.api.Assertions._
+import org.junit.jupiter.api.Test
+
+object Neg2b {
+
+}
