320e9a0ca8e6fd19ef0a7806b8ea3b32b596010d
Fabio Muratori <fabio.muratori2@studio.unibo.it>
2021-03-12 11:32:03 +0100

    Implement and test second part of task 2b, refactor duplicated code

--- src/u02_lab/Neg2b.scala
+++ src/u02_lab/Neg2b.scala
@@ -1,18 +1,28 @@
 package u02_lab
 
 import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.{Test}
 
 
 object Neg2b {
 
-  def neg(f: String=>Boolean): String=>Boolean = s => !f(s)
+  def negMethod(f: String=>Boolean): String=>Boolean = s => !f(s)
 
+  val negLambda: (String=>Boolean) => (String=>Boolean) = f => s => !f(s)
 
-  @Test def testParityFunction(){
-    val empty: String => Boolean = _==""
-    val notEmpty = neg(empty)
+  /////////////////////////////////////////////////////////////////////////////
 
+  val empty: String => Boolean = _==""
+
+  @Test def testNegMethod(){
+    notTestAssert(negMethod(empty));
+  }
+
+  @Test def testNegLambda() {
+    notTestAssert(negLambda(empty));
+  }
+
+  def notTestAssert(notEmpty: String=>Boolean): Unit = {
     assertEquals(true, notEmpty("foo"))
     assertEquals(false, notEmpty(""))
     assertEquals(true, notEmpty("foo") && !notEmpty(""))
