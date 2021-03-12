d3aa81f36ad9972b5cd76121095fd01ea47b3503
Fabio Muratori <fabio.muratori2@studio.unibo.it>
2021-03-12 11:08:37 +0100

    Test and implement task 2b

--- src/u02_lab/Neg2b.scala
+++ src/u02_lab/Neg2b.scala
@@ -1,8 +1,21 @@
 package u02_lab
 
-import org.junit.jupiter.api.Assertions._
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
+
 object Neg2b {
+
+  def neg(f: String=>Boolean): String=>Boolean = s => !f(s)
+
+
+  @Test def testParityFunction(){
+    val empty: String => Boolean = _==""
+    val notEmpty = neg(empty)
+
+    assertEquals(true, notEmpty("foo"))
+    assertEquals(false, notEmpty(""))
+    assertEquals(true, notEmpty("foo") && !notEmpty(""))
+  }
 
 }
